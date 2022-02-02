6cbe118c1d799b1e15192b9f6facbe09f95e1b35
m3gagluk <15657215+m3gagluk@users.noreply.github.com>
2022-02-02 19:05:40 +0300

    breaking: updated the memory file structure to the one returned by Varjo API

--- VarjoCompanion/MainApp.cs
+++ VarjoCompanion/MainApp.cs
@@ -8,53 +8,6 @@ using System.Threading.Tasks;
 
 namespace VarjoCompanion
 {
-    [StructLayout(LayoutKind.Sequential)]
-    public struct MemoryEye
-    {
-        public bool opened;
-        public double pupilSize;
-        public double x;
-        public double y;
-
-        private void CopyFrom(VarjoEyeTracking.GazeRay ray, double pupil)
-        {
-            VarjoEyeTracking.Vector forward = ray.forward;
-            x = forward.x;
-            y = forward.y;
-            pupilSize = pupil;
-        }
-
-        public void CopyFrom(VarjoEyeTracking.GazeRay ray, double pupil, VarjoEyeTracking.GazeEyeStatus status)
-        {
-            this.CopyFrom(ray, pupil);
-            opened = status != VarjoEyeTracking.GazeEyeStatus.Invalid;
-        }
-
-        public void CopyFrom(VarjoEyeTracking.GazeRay ray, double pupil, VarjoEyeTracking.GazeStatus status)
-        {
-            this.CopyFrom(ray, pupil);
-            opened = status != VarjoEyeTracking.GazeStatus.Invalid;
-        }
-    }
-
-    [StructLayout(LayoutKind.Sequential)]
-    public struct MemoryData
-    {
-        public bool shutdown;
-        public bool calibrated;
-        public MemoryEye leftEye;
-        public MemoryEye rightEye;
-        public MemoryEye combined;
-
-        public void CopyFrom(VarjoEyeTracking.GazeData data)
-        {
-            calibrated = data.status != VarjoEyeTracking.GazeStatus.Invalid;
-            leftEye.CopyFrom(data.leftEye, data.leftPupilSize, data.leftStatus);
-            rightEye.CopyFrom(data.rightEye, data.rightPupilSize, data.rightStatus);
-            combined.CopyFrom(data.gaze, (data.leftPupilSize + data.rightPupilSize) / 2, data.status);
-        }
-    }
-
     class MainApp
     {
         static void Main(string[] args)
@@ -70,19 +23,17 @@ namespace VarjoCompanion
             VarjoEyeTracking.GazeInit();
             VarjoEyeTracking.SyncProperties();
 
-            MemoryData memoryData = new MemoryData();
+            VarjoEyeTracking.GazeData gazeData = new VarjoEyeTracking.GazeData();
 
-            using (var memMapFile = MemoryMappedFile.CreateNew("VarjoEyeTracking", Marshal.SizeOf(memoryData)))
+            using (var memMapFile = MemoryMappedFile.CreateNew("VarjoEyeTracking", Marshal.SizeOf(gazeData)))
             {
                 using (var accessor = memMapFile.CreateViewAccessor())
                 {
-                    VarjoEyeTracking.GazeData gazeData;
                     Console.WriteLine("Eye tracking session has started!");
                     while (!(Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Enter))
                     {
                         gazeData = VarjoEyeTracking.GetGaze();
-                        memoryData.CopyFrom(gazeData);
-                        accessor.Write(0, ref memoryData);
+                        accessor.Write(0, ref gazeData);
                     }
                 }
             }
